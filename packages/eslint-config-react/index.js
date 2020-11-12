b6d8f8d7218d3c5ad1bf950561f88e46eb9521c7
odelucca <delucca@weme.nu>
2020-11-12 20:50:57 -0300

    fix

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ["xo-react", "xo-react/space"],
+  rules: {
+    "react/jsx-tag-spacing": [
+      "error",
+      {
+        closingSlash: "never",
+        beforeSelfClosing: "always",
+        afterOpening: "never",
+        beforeClosing: "never",
+      },
+  },
 }
